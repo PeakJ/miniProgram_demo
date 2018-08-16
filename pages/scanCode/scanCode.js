4c1d8cd4a2e7d6eba9005817a429c50f37c8e833
jiangfeng <jiangfeng@yongche.com>
2018-08-16 18:30:50 +0800

    修改输出参数

--- pages/scanCode/scanCode.js
+++ pages/scanCode/scanCode.js
@@ -1,37 +1,37 @@
-
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-scanCode: function () {
-  wx.scanCode({
-    onlyFromCamera:true,
-    scanType: ['qrCode','barCode'],
-    success: function (data) {
-      if(data.scanType==='QR_CODE'){
-        wx.showToast({
-          title: '其实我想打开，但是不会',
-          icon: 'none'
-        })
-      }else{
-        wx.showToast({
-          title:'仅仅是扫了一下',
-          icon: 'none'
-        })
-      }
-      console.log(result);
-      
+    /**
+     * 页面的初始数据
+     */
+    data: {},
+    onLoad: () => {
+        console.log(app.globalData);
     },
-    fail: function(error){
-      console.log(error);
-      wx.showToast({
-        title: error
-      })
+    scanCode: function () {
+        wx.scanCode({
+            onlyFromCamera: true,
+            scanType: ['qrCode', 'barCode'],
+            success: function (data) {
+                if (data.scanType === 'QR_CODE') {
+                    wx.showToast({
+                        title: '其实我想打开，但是不会',
+                        icon: 'none'
+                    })
+                } else {
+                    wx.showToast({
+                        title: '仅仅是扫了一下',
+                        icon: 'none'
+                    })
+                }
+                console.log(data);
+
+            },
+            fail: function (error) {
+                console.log(error);
+                wx.showToast({
+                    title: error.errMsg
+                })
+            }
+        })
     }
-  })
-}
-})
+});
